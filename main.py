c5cbb281818dffd1217d4069a66cd01f12d2315f
Lukáš Hellmann <hellmann@vshosting.cz>
2022-11-24 12:39:13 +0100

    python probe

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import platform
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello from Zerops!'
+
+
+@app.route('/test')
+def test():
+    return 'test'
+
+
+@app.route('/version')
+def version():
+    return platform.python_version()
+
+
+app.run(host='0.0.0.0', port=8080)
