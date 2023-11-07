0de456e365275165110dee11d7971c9752941315
itpnsk <91492136+itpnsk@users.noreply.github.com>
2023-11-07 14:13:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
+# from flask import Flask, render_template
+# import platform
+# import datetime
+
+# app = Flask(__name__)
+
+# a = 10
+
+# @app.route('/')
+# def index():
+#     return '<h1>Hello from Zerops!</h1> TEST5 {{a}}'
+
+# @app.route('/test')
+# def test():
+#     return 'test'
+
+
+# @app.route('/version')
+# def version():
+#     return platform.python_version()
+
 from flask import Flask, render_template
-import platform
-import datetime
 
 app = Flask(__name__)
 
-a = 10
-
 @app.route('/')
-def index():
-    return '<h1>Hello from Zerops!</h1> TEST5 {{a}}'
-
-@app.route('/test')
-def test():
-    return 'test'
-
-
-@app.route('/version')
-def version():
-    return platform.python_version()
-
+def home():
+    fruits = ['apple', 'orange', 'pear', 'pineapple', 'durian']
+    return render_template(
+        'index.html',
+        name='bob',
+        age=40,
+        fruits=fruits
+    )
 
 app.run(host='0.0.0.0', port=8080)
