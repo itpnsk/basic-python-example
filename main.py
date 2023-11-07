14271691865a088cf66544da186f69475b5c2848
itpnsk <91492136+itpnsk@users.noreply.github.com>
2023-11-07 15:09:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# from flask import Flask, render_template
-# import platform
-# import datetime
+from flask import Flask, render_template
+import platform
+import datetime
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
 # a = 10
 
-# @app.route('/')
-# def index():
-#     return '<h1>Hello from Zerops!</h1> TEST5 {{a}}'
+@app.route('/')
+def index():
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
 # @app.route('/test')
 # def test():
@@ -19,12 +19,12 @@
 # def version():
 #     return platform.python_version()
 
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return '<h1>Hello from Zerops!</h1> TEST5'
+# @app.route('/')
+# def home():
+#     return '<h1>Hello from Zerops!</h1> TEST5'
 
 app.run(host='0.0.0.0', port=8080)
