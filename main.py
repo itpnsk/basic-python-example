670c96166fab699da477a92d652011c66be0c5f9
itpnsk <91492136+itpnsk@users.noreply.github.com>
2023-11-07 15:12:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
-from flask import Flask, render_template
-import platform
-import datetime
-
-app = Flask(__name__)
-
-# a = 10
-
-@app.route('/')
-def index():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
-
-# @app.route('/test')
-# def test():
-#     return 'test'
-
-
-# @app.route('/version')
-# def version():
-#     return platform.python_version()
-
 # from flask import Flask, render_template
+# import platform
+# import datetime
 
 # app = Flask(__name__)
 
+# # a = 10
+
 # @app.route('/')
-# def home():
-#     return '<h1>Hello from Zerops!</h1> TEST5'
+# def index():
+#     return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+
+# # @app.route('/test')
+# # def test():
+# #     return 'test'
+
+
+# # @app.route('/version')
+# # def version():
+# #     return platform.python_version()
+
+# # from flask import Flask, render_template
+
+# # app = Flask(__name__)
+
+# # @app.route('/')
+# # def home():
+# #     return '<h1>Hello from Zerops!</h1> TEST5'
+
+# app.run(host='0.0.0.0', port=8080)
+
+import datetime
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
 app.run(host='0.0.0.0', port=8080)
